7a59a781a41cca67ea9ab0aa7764e3a3e8ec2480
tofstricker <69083547+tofstricker@users.noreply.github.com>
2026-04-26 21:49:30 +0300

    refactor: Update Kotlin plugin and Flutter Activity base class
    Updates the Kotlin Gradle plugin ID from `kotlin-android` to `org.jetbrains.kotlin.android` for both child and parent apps. Also, changes the base class for `MainActivity` from `FlutterFragmentActivity` to `FlutterActivity`.
    
    Additionally, adds `flutter clean` commands to the CI workflow to ensure a clean build of the APKs.

--- flutter_child_app/android/app/src/main/kotlin/com/example/kitecontrol/MainActivity.kt
+++ flutter_child_app/android/app/src/main/kotlin/com/example/kitecontrol/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.kitecontrol
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
     private val USAGE_CHANNEL = "com.example.kitecontrol/usage"
     private val BLOCKING_CHANNEL = "com.example.kitecontrol/blocking"
 
